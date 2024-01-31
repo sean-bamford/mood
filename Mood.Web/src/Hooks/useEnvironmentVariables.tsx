cac8a0e94f5fc4fa65e0059a555a62947b2d1109
sean-bamford <97364673+sean-bamford@users.noreply.github.com>
2024-01-31 10:40:00 -0500

    updated AuraDB instance

--- Mood.Web/src/Hooks/useEnvironmentVariables.tsx
+++ Mood.Web/src/Hooks/useEnvironmentVariables.tsx
@@ -3,9 +3,15 @@
 // const password = "tesseract"
 // const login = {uri, user, password}
 
-const uri= "neo4j://5e024b2f.databases.neo4j.io" //AuraDB cloud instance variables
+const uri= "neo4j+s://70b71cb8.databases.neo4j.io" //AuraDB cloud instance variables
 const user = "neo4j"
-const password = "ibJM01X5wMmOu96GxY66TZAyD3FG-4z3yr5Fu6n1hak"
+const password = "r_vp9dplHkK-kOtejJaIzM4RwFHWaHrYSBQ44lLs37s"
 const login = {uri, user, password}
 export default login;
 
+// # Wait 60 seconds before connecting using these details, or login to https://console.neo4j.io to validate the Aura Instance is available
+// NEO4J_URI=neo4j+s://70b71cb8.databases.neo4j.io
+// NEO4J_USERNAME=neo4j
+// NEO4J_PASSWORD=r_vp9dplHkK-kOtejJaIzM4RwFHWaHrYSBQ44lLs37s
+// AURA_INSTANCEID=70b71cb8
+// AURA_INSTANCENAME=Instance01
